6ff7fd445bfb77bc96c34bc1109bd630b0a3305c
Fodor Gábor Dániel <dani9417@gmail.com>
2019-04-25 20:31:19 +0200

    starter google login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
+import jwt from 'jsonwebtoken';
 
 class App extends Component {
+  responseGoogle = async (res) => {
+    console.log(res);
+    const tokenBlob = new Blob(
+      [JSON.stringify({ access_token: res.accessToken }, null, 2)],
+      { type: 'application/json' },
+    );
+    const options = {
+      method: 'POST',
+      body: tokenBlob,
+    };
+    const response = await fetch('https://biofeedback-api.herokuapp.com/auth/google', options);
+    const body = await response.json();
+    console.log(jwt.decode(body.jwt, 'asdfsadfasdfasdf'));
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={console.log}
+          responseType="token"
+          cookiePolicy="single_host_origin"
+          scope="https://www.googleapis.com/auth/plus.login"
+        />
+
+        <GoogleLogout
+          clientId={process.env.REACT_APP_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={console.log}
+        />
       </div>
     );
   }
